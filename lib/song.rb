544c4895288ec5bf96a6a23ceb620c88a6f1a917
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 00:53:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,18 +6,6 @@ class Song
    @artist=artist
    @genre=genre
 end
-
-def song=(name, artist, genre)
-   @name=name
-   @artist=artist
-   @genre=genre
- end
- 
- def song
-   @name
-   @artist
-   @genre
- end
  
  @@count = 0
 
