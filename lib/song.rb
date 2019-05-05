e04f4d74586bcbaff7b6660100273b3e0d5d9a88
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 00:44:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,23 +2,21 @@ class Song
  attr_accessor :name, :artist, :genre
  
  def initialize(name, artist, genre)
-   @name=name
-   @artist=artist
-   @genre=genre
+   @@name=name
+   @@artist=artist
+   @@genre=genre
 end
 
 def song=(name, artist, genre)
-    @name=name
-   @artist=artist
-   @genre=genre
+    @@name=name
+   @@artist=artist
+   @@genre=genre
  end
  
  def song
-   @name
-   @artist
-   @genre
+   @@name
+   @@artist
+   @@genre
  end
- 
- @@count = 1 
  end
  
